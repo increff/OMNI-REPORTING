00a7babb017bd45900c334fc6fa99d61cdf5643a
Viram132 <viram.shah@increff.com>
2023-08-10 13:38:35 +0530

    changed column type to longtext

--- src/main/java/com/increff/omni/reporting/pojo/ReportQueryPojo.java
+++ src/main/java/com/increff/omni/reporting/pojo/ReportQueryPojo.java
@@ -19,8 +19,7 @@ public class ReportQueryPojo extends AbstractVersionedPojo{
     @Column(nullable = false, unique = true)
     private Integer reportId;
 
-    @Lob
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
     private String query;
 
 }
