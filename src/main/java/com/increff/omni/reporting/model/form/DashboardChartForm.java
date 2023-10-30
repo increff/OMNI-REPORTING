865ee80da9608072db508c8dde6d57ee23beee15
abhaychandna <email@example.com>
2023-10-30 09:08:57 +0530

    DashboardChartForm / Data

---
+++ src/main/java/com/increff/omni/reporting/model/form/DashboardChartForm.java
@@ -0,0 +1,22 @@
+package com.increff.omni.reporting.model.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+public class DashboardChartForm {
+    @NotNull
+    private Integer dashboardId;
+    @NotNull
+    private String chartAlias;
+    @NotNull
+    private Integer row;
+    @NotNull
+    private Integer col;
+    @NotNull
+    private Integer col_width;
+    //TODO : Add filters list? or just get from chartAlias
+}
