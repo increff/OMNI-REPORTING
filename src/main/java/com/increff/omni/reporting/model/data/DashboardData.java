bb4885dc5b5f5c89437021821207f6bd8c78c537
abhaychandna <email@example.com>
2023-10-30 13:10:24 +0530

    dahsboard data

---
+++ src/main/java/com/increff/omni/reporting/model/data/DashboardData.java
@@ -0,0 +1,17 @@
+package com.increff.omni.reporting.model.data;
+
+import com.increff.omni.reporting.model.form.DashboardForm;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class DashboardData {
+
+    private Integer id;
+    private DashboardForm dashboardDetails;
+    private List<InputControlData> filterDetails;
+    private List<List<DashboardChartData>> grid;
+}
