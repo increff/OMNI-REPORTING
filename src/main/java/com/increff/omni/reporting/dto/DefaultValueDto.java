9071f5609b3b5238c4e5c43ef9acaeaba35253a3
abhaychandna <email@example.com>
2023-11-02 11:44:45 +0530

    validateControlIdExistsForDashboard

--- src/main/java/com/increff/omni/reporting/dto/DefaultValueDto.java
+++ src/main/java/com/increff/omni/reporting/dto/DefaultValueDto.java
@@ -1,15 +1,19 @@
 package com.increff.omni.reporting.dto;
 
 import com.increff.omni.reporting.api.DashboardApi;
+import com.increff.omni.reporting.api.DashboardChartApi;
 import com.increff.omni.reporting.api.DefaultValueApi;
 import com.increff.omni.reporting.model.data.DefaultValueData;
+import com.increff.omni.reporting.model.data.InputControlData;
 import com.increff.omni.reporting.model.form.DefaultValueForm;
 import com.increff.omni.reporting.pojo.DefaultValuePojo;
 import com.nextscm.commons.spring.common.ApiException;
+import com.nextscm.commons.spring.common.ApiStatus;
 import com.nextscm.commons.spring.common.ConvertUtil;
 import io.swagger.models.auth.In;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,18 +29,31 @@ public class DefaultValueDto extends AbstractDto {
     private DefaultValueApi api;
     @Autowired
     private DashboardApi dashboardApi;
+    @Autowired
+    private DashboardDto dashboardDto;
+    @Autowired
+    private DashboardChartApi dashboardChartApi;
 
     public List<DefaultValueData> upsert(List<DefaultValueForm> forms) throws ApiException {
         List<DefaultValuePojo> pojos = new ArrayList<>();
         for(DefaultValueForm form : forms) {
             checkValid(form);
             dashboardApi.getCheck(form.getDashboardId(), getOrgId());
-            // TODO: Check if control id exists for that dashboard id
+            validateControlIdExistsForDashboard(form.getDashboardId(), form.getControlId());
+
             DefaultValuePojo pojo = ConvertUtil.convert(form, DefaultValuePojo.class);
             pojo.setDefaultValue(String.join(",", form.getDefaultValue()));
             pojos.add(api.upsert(pojo));
         }
         return ConvertUtil.convert(pojos, DefaultValueData.class);
+    }
+
+    private void validateControlIdExistsForDashboard(Integer dashboardId, Integer controlId) throws ApiException {
+        List<InputControlData> inputControlDatas = dashboardDto.getFilterDetails(getOrgId(), dashboardApi.getCheck(dashboardId, getOrgId()),
+                dashboardChartApi.getByDashboardId(dashboardId));
+        if(inputControlDatas.stream().noneMatch(inputControlData -> inputControlData.getId().equals(controlId))){
+            throw new ApiException(ApiStatus.BAD_DATA, "Control Id does not exist for dashboard id: " + dashboardId + " control id: " + controlId);
+        }
     }
 
 
