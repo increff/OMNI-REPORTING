bc7b44e570735f1799f52dcfff5f333978370fdd
abhaychandna <email@example.com>
2023-11-06 15:26:55 +0530

    legends dto

---
+++ src/main/java/com/increff/omni/reporting/dto/ChartLegendsDto.java
@@ -0,0 +1,31 @@
+package com.increff.omni.reporting.dto;
+
+import com.increff.omni.reporting.api.ChartLegendsApi;
+import com.increff.omni.reporting.model.data.ChartLegendsData;
+import com.increff.omni.reporting.model.form.ChartLegendsForm;
+import com.increff.omni.reporting.pojo.ChartLegendsPojo;
+import com.nextscm.commons.spring.common.ApiException;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.increff.omni.reporting.util.ConvertUtil.convertChartLegendsPojoToChartLegendsData;
+
+@Service
+@Log4j
+@Setter
+public class ChartLegendsDto extends AbstractDto {
+
+    @Autowired
+    private ChartLegendsApi api;
+
+    public ChartLegendsData put(Integer chartId, ChartLegendsForm form) throws ApiException {
+        checkValid(form);
+        List<ChartLegendsPojo> pojos = api.put(chartId, form.getLegends());
+        return convertChartLegendsPojoToChartLegendsData(pojos);
+    }
+
+}
