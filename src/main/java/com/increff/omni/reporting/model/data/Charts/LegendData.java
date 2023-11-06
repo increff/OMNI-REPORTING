53683ce30a05aa7709e73fd32560b6718e7ae7a6
abhaychandna <email@example.com>
2023-11-06 10:51:09 +0530

    LegendData

---
+++ src/main/java/com/increff/omni/reporting/model/data/Charts/LegendData.java
@@ -0,0 +1,12 @@
+package com.increff.omni.reporting.model.data.Charts;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class LegendData {
+    Map<String, String> legend;
+}
