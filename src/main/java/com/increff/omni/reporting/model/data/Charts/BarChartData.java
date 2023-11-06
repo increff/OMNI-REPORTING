75cf2d1b28d464f5d29f2cd6a1dd6d55ca3931e4
abhaychandna <email@example.com>
2023-11-06 10:54:45 +0530

    todo

--- src/main/java/com/increff/omni/reporting/model/data/Charts/BarChartData.java
+++ src/main/java/com/increff/omni/reporting/model/data/Charts/BarChartData.java
@@ -4,10 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
 public class BarChartData {
-    List<String> labels;
-    List<Integer> data;
+    Map<String, String> data;
+    // TODO: Add legends
 }
