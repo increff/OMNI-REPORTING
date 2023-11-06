2f33cd7ef1d5520bff1eafd6435cfbee50b68842
abhaychandna <email@example.com>
2023-11-06 13:36:34 +0530

    api

---
+++ src/main/java/com/increff/omni/reporting/api/ChartLegendsApi.java
@@ -0,0 +1,54 @@
+package com.increff.omni.reporting.api;
+
+import com.increff.omni.reporting.dao.ChartLegendsDao;
+import com.increff.omni.reporting.pojo.ChartLegendsPojo;
+import com.nextscm.commons.spring.common.ApiException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class ChartLegendsApi extends AbstractAuditApi {
+
+    @Autowired
+    private ChartLegendsDao dao;
+
+    public ChartLegendsPojo add(ChartLegendsPojo pojo) throws ApiException {
+        dao.persist(pojo);
+        return pojo;
+    }
+
+    public List<ChartLegendsPojo> put(Integer chartId, Map<String, String> map) throws ApiException {
+        List<ChartLegendsPojo> pojos = new ArrayList<>();
+        if(Objects.isNull(map))return pojos;
+
+        deleteByChartId(chartId); // Delete Existing Legends
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            ChartLegendsPojo pojo = new ChartLegendsPojo();
+            pojo.setChartId(chartId);
+            pojo.setLegendKey(entry.getKey());
+            pojo.setValue(entry.getValue());
+            add(pojo);
+            pojos.add(pojo);
+        }
+        return pojos;
+    }
+
+    public List<ChartLegendsPojo> getByChartId(Integer chartId) {
+        return dao.getByChartId(chartId);
+    }
+
+    public void deleteByChartId(Integer chartId) {
+        List<ChartLegendsPojo> pojos = getByChartId(chartId);
+        for(ChartLegendsPojo pojo : pojos){
+            dao.remove(pojo);
+        }
+    }
+
+}
