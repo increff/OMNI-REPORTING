868981bfa372cf154ca84638c1b0b6c8e8ca1abe
abhaychandna <email@example.com>
2023-11-07 10:47:24 +0530

    bar chart impl

---
+++ src/main/java/com/increff/omni/reporting/model/data/Charts/BarChartImpl.java
@@ -0,0 +1,20 @@
+package com.increff.omni.reporting.model.data.Charts;
+
+
+import com.increff.omni.reporting.model.constants.ChartType;
+import com.nextscm.commons.spring.common.ApiException;
+
+import java.util.List;
+import java.util.Map;
+
+public class BarChartImpl implements ChartInterface {
+
+    public Object transform(List<Map<String, String>> result) throws ApiException {
+        System.out.println("BarData");
+        BarChartData data = new BarChartData();
+//        data.setLabels(new ArrayList<>(result.get(0).keySet()));
+//        data.setData(result.get(0).values().stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()));
+//        data.setData(result.get(0));
+        return data;
+    }
+}
