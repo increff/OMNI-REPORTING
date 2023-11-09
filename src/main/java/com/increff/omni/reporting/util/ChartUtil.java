bd143a0caa01198ce79d56ac7807b096f9c3fef5
abhaychandna <email@example.com>
2023-11-09 12:18:35 +0530

    getMapMultiValueChartData

--- src/main/java/com/increff/omni/reporting/util/ChartUtil.java
+++ src/main/java/com/increff/omni/reporting/util/ChartUtil.java
@@ -1,17 +1,46 @@
 package com.increff.omni.reporting.util;
 
-import com.increff.omni.reporting.model.data.Charts.GenericChartData;
+import com.increff.omni.reporting.model.data.Charts.MapMultiValueChartData;
+import com.increff.omni.reporting.model.data.Charts.MapSingleValueChartData;
 import lombok.extern.log4j.Log4j;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Log4j
 public class ChartUtil {
 
-    public static GenericChartData getGenericChartData(List<Map<String, String>> result) {
-        GenericChartData chartData = new GenericChartData();
+    public static MapSingleValueChartData getGenericChartData(List<Map<String, String>> result) {
+        MapSingleValueChartData chartData = new MapSingleValueChartData();
         chartData.setData(result.get(0));
         return chartData;
+    }
+
+
+    public static MapMultiValueChartData getMapMultiValueChartData(List<Map<String, String>> result) {
+        MapMultiValueChartData chart = new MapMultiValueChartData();
+        // first column values is treated as labels
+        // each column is a dataset
+        // each row has 1 value for each dataset and the label corresponding to that value
+        Map<String, List<String>> data = new HashMap<>();
+        List<String> columns = new ArrayList<>(result.get(0).keySet());
+        String labelColumn = columns.get(0);
+        List<String> valueColumns = columns.subList(1, columns.size());
+
+        data.put("labels", new ArrayList<>());
+        for (String valueColumn : valueColumns) {
+            data.put(valueColumn, new ArrayList<>());
+        }
+
+        for (Map<String, String> row : result) {
+            data.get("labels").add(row.get(labelColumn));
+            for (String valueColumn : valueColumns) {
+                data.get(valueColumn).add(row.get(valueColumn));
+            }
+        }
+        chart.setData(data);
+        return chart;
     }
 }
