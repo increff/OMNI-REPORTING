cb1f5d6b8e2bf24cce405e8e32f08d381e755b07
abhaychandna <email@example.com>
2023-11-09 11:07:25 +0530

    MapSingleValueChartDataImpl

---
+++ src/main/java/com/increff/omni/reporting/model/data/Charts/MapSingleValueChartDataImpl.java
@@ -0,0 +1,13 @@
+package com.increff.omni.reporting.model.data.Charts;
+
+
+import java.util.List;
+import java.util.Map;
+
+import static com.increff.omni.reporting.util.ChartUtil.getGenericChartData;
+
+public class MapSingleValueChartDataImpl implements ChartInterface {
+    public Object transform(List<Map<String, String>> result){
+        return getGenericChartData(result);
+    }
+}
