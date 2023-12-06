44b52a051e15147e913a9c96d986258dceb53fb4
abhaychandna <email@example.com>
2023-12-06 11:16:57 +0530

    hsotname

--- src/main/java/com/increff/omni/reporting/job/ScheduledJobs.java
+++ src/main/java/com/increff/omni/reporting/job/ScheduledJobs.java
@@ -127,7 +127,7 @@ public class ScheduledJobs {
 
     private String getIp() {
         try {
-            return InetAddress.getLocalHost().getHostAddress();
+            return InetAddress.getLocalHost().getHostAddress() + "_" + InetAddress.getLocalHost().getHostName();
         } catch (UnknownHostException e) {
             log.error("Error while getting ip address", e);
             return "Error while getting ip address " + e;
