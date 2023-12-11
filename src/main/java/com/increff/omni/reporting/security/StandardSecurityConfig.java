15e3606fff3182554b15055f6c9f3be15bf32d4b
abhaychandna <email@example.com>
2023-12-11 16:10:07 +0530

    security added

--- src/main/java/com/increff/omni/reporting/security/StandardSecurityConfig.java
+++ src/main/java/com/increff/omni/reporting/security/StandardSecurityConfig.java
@@ -4,6 +4,7 @@ import com.increff.account.client.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +34,9 @@ public class StandardSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/standard/**").and().authorizeRequests()//
                 .antMatchers("/standard/schedules/**").hasAnyAuthority(APP_ADMIN)
                 .antMatchers("/standard/**").hasAnyAuthority(APP_ADMIN, REPORT_ADMIN, REPORT_STANDARD)//
+                .antMatchers("/standard/dashboards/**").hasAnyAuthority(APP_ADMIN, REPORT_ADMIN)//
+                .antMatchers(HttpMethod.GET, "/standard/dashboards/**").hasAnyAuthority(APP_ADMIN, REPORT_ADMIN, REPORT_STANDARD)//
+                .antMatchers("/standard/dashboards/{dashboardId}/view").hasAnyAuthority(APP_ADMIN, REPORT_ADMIN, REPORT_STANDARD)//
                 .and().cors().and().csrf().disable()
                 .addFilterBefore(authTokenFilter, BasicAuthenticationFilter.class)
                 .addFilterAfter(rateLimitingFilter, AuthTokenFilter.class)
