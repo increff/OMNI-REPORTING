74125adb121d2535ff92170a5356a7ab01f2a947
abhaychandna <email@example.com>
2024-02-14 09:08:59 +0530

    escape backslash removed

--- src/main/java/com/increff/omni/reporting/util/UserPrincipalUtil.java
+++ src/main/java/com/increff/omni/reporting/util/UserPrincipalUtil.java
@@ -85,7 +85,6 @@ public class UserPrincipalUtil {
                 continue;
             }
             s = s.replace("'", "\\'");
-            s = s.replace("\\", "\\\\"); // replaces single backslash with double backslash
             fList.add("'" + s + "'");
         }
         finalMap.put(key, String.join(",", fList));
