a7a5a0112fa2370ee960c8de6e0e1d63fb6d4b5f
abhaychandna <email@example.com>
2024-02-20 21:24:40 +0530

    getByAliasAndSchema used for getCheckByAliasAndSchema instead of directly adding in dao

--- src/main/java/com/increff/omni/reporting/api/ReportApi.java
+++ src/main/java/com/increff/omni/reporting/api/ReportApi.java
@@ -113,7 +113,7 @@ public class ReportApi extends AbstractAuditApi {
     }
 
     public ReportPojo getCheckByAliasAndSchema(String alias, Integer schemaVersionId, Boolean isChart) throws ApiException {
-        ReportPojo pojo = dao.getByAliasAndSchema(alias, schemaVersionId, isChart);
+        ReportPojo pojo = getByAliasAndSchema(alias, schemaVersionId, isChart);
         checkNotNull(pojo, "No report present with alias : " + alias + " schemaVersionId : " + schemaVersionId + " isChart : " + isChart);
         return pojo;
     }
