6e1f15ae21f3dd45a94452f904a4a41b84455864
abhaychandna <email@example.com>
2024-02-22 15:21:46 +0530

    db type added

--- src/main/java/com/increff/omni/reporting/pojo/ConnectionPojo.java
+++ src/main/java/com/increff/omni/reporting/pojo/ConnectionPojo.java
@@ -1,5 +1,6 @@
 package com.increff.omni.reporting.pojo;
 
+import com.increff.omni.reporting.model.constants.DBType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,6 @@ public class ConnectionPojo extends AbstractVersionedPojo{
     private String username;
     @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
+    private DBType dbType;
 }
