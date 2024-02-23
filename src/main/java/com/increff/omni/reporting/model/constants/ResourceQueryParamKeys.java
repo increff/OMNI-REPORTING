dafefdbf38566f33c6cb9d63185a49b625197778
abhaychandna <email@example.com>
2024-02-23 09:29:04 +0530

    constants

--- src/main/java/com/increff/omni/reporting/model/constants/ResourceQueryParamKeys.java
+++ src/main/java/com/increff/omni/reporting/model/constants/ResourceQueryParamKeys.java
@@ -4,4 +4,6 @@ public class ResourceQueryParamKeys {
     public final static String fulfillmentLocationQueryParamKey = "user.access.fulfillmentLocations";
     public final static String clientQueryParam = "user.access.clients";
     public final static String restrictedResourceQueryParam = "user.access.restricted-resource";
+
+    public final static String orgIdQueryParamKey = "user.access.orgId";
 }
