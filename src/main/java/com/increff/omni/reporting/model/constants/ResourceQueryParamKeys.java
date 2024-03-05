06c17bd269d9cb17aa1ff3abac8c62a6d010eab6
abhaychandna <email@example.com>
2024-03-05 10:31:07 +0530

    unsued key removed

--- src/main/java/com/increff/omni/reporting/model/constants/ResourceQueryParamKeys.java
+++ src/main/java/com/increff/omni/reporting/model/constants/ResourceQueryParamKeys.java
@@ -4,6 +4,4 @@ public class ResourceQueryParamKeys {
     public final static String fulfillmentLocationQueryParamKey = "user.access.fulfillmentLocations";
     public final static String clientQueryParam = "user.access.clients";
     public final static String restrictedResourceQueryParam = "user.access.restricted-resource";
-
-    public final static String orgIdQueryParamKey = "user.access.orgId";
 }
