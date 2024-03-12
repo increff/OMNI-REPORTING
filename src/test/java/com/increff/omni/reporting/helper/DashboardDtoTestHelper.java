3f4e516d346f64aac402c188f0f44a8e5040445a
abhaychandna <email@example.com>
2024-03-12 11:57:31 +0530

    test helper updated

---
+++ src/test/java/com/increff/omni/reporting/helper/DashboardDtoTestHelper.java
@@ -0,0 +1,51 @@
+package com.increff.omni.reporting.helper;
+
+import com.increff.omni.reporting.model.constants.RowHeight;
+import com.increff.omni.reporting.model.form.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DashboardDtoTestHelper {
+
+    public static DashboardAddForm getDashboardAddForm(String name, List<DashboardChartForm> charts) {
+        DashboardAddForm form = new DashboardAddForm();
+        form.setName(name);
+        form.setCharts(charts);
+        return form;
+    }
+
+
+    public static DashboardChartForm getDashboardChartForm(String chartAlias, Integer row, Integer col, Integer colWidth, RowHeight rowHeight) {
+        DashboardChartForm form = new DashboardChartForm();
+        form.setChartAlias(chartAlias);
+        form.setRow(row);
+        form.setCol(col);
+        form.setColWidth(colWidth);
+        form.setRowHeight(rowHeight);
+        return form;
+    }
+
+    public static DashboardForm getDashboardForm(String name) {
+        DashboardForm form = new DashboardForm();
+        form.setName(name);
+        return form;
+    }
+
+    public static DefaultValueForm getDefaultValueForm(Integer dashboardId, String paramName, List<String> defaultValue) {
+        DefaultValueForm form = new DefaultValueForm();
+        form.setDashboardId(dashboardId);
+        form.setParamName(paramName);
+        form.setDefaultValue(defaultValue);
+        return form;
+    }
+
+    public static UpsertDefaultValueForm getUpsertDefaultValueForm(Integer dashboardId, String paramName, List<String> defaultValue) {
+        UpsertDefaultValueForm form = new UpsertDefaultValueForm();
+        form.setDefaultValueForms(Arrays.asList(getDefaultValueForm(dashboardId, paramName, defaultValue)));
+        form.setValidationGroupsValueForms(Collections.emptyList());
+        return form;
+    }
+}
