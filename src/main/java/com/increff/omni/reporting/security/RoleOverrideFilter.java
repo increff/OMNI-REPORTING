b88cd84bdc02ded8d3558ec9d95a984cf5653a68
abhaychandna <email@example.com>
2024-03-15 14:13:43 +0530

    RoleOverrideFilter

--- src/main/java/com/increff/omni/reporting/security/RoleOverrideFilter.java
+++ src/main/java/com/increff/omni/reporting/security/RoleOverrideFilter.java
@@ -4,6 +4,7 @@ import com.increff.account.client.SecurityUtil;
 import com.increff.account.client.UserPrincipal;
 import com.increff.omni.reporting.model.constants.Roles;
 import lombok.extern.log4j.Log4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -23,17 +24,24 @@ public class RoleOverrideFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         UserPrincipal userPrincipal = SecurityUtil.getPrincipal();
+
         if(Objects.nonNull(userPrincipal)) {
             List<String> roles = userPrincipal.getRoles();
             log.debug("Roles before override: " + roles);
             if (roles.stream().anyMatch(role -> role.equalsIgnoreCase(Roles.REPORT_STANDARD.getRole()))) {
-                if (roles.stream().noneMatch(role -> role.equalsIgnoreCase(Roles.OMNI_REPORT_STANDARD.getRole())))
+                if (roles.stream().noneMatch(role -> role.equalsIgnoreCase(Roles.OMNI_REPORT_STANDARD.getRole()))) {
                     roles.add(Roles.OMNI_REPORT_STANDARD.getRole()); // Add role to userPrincipal if not present
+                    SimpleGrantedAuthority authority = new SimpleGrantedAuthority(Roles.OMNI_REPORT_STANDARD.getRole());
+                    SecurityUtil.addAuthority(authority);
+                }
             }
 
             if (roles.stream().anyMatch(role -> role.equalsIgnoreCase(Roles.REPORT_CUSTOM.getRole()))) {
-                if (roles.stream().noneMatch(role -> role.equalsIgnoreCase(Roles.OMNI_REPORT_CUSTOM.getRole())))
+                if (roles.stream().noneMatch(role -> role.equalsIgnoreCase(Roles.OMNI_REPORT_CUSTOM.getRole()))) {
                     roles.add(Roles.OMNI_REPORT_CUSTOM.getRole()); // Add role to userPrincipal if not present
+                    SimpleGrantedAuthority authority = new SimpleGrantedAuthority(Roles.OMNI_REPORT_CUSTOM.getRole());
+                    SecurityUtil.addAuthority(authority);
+                }
             }
             userPrincipal.setRoles(roles);
             log.debug("Roles after override: " + userPrincipal.getRoles());
