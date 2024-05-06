58e9656845828c76cf2c2bdf69db9522cbda1c30
rishavraj <rishav.raj@increff.com>
2024-05-06 14:13:17 +0530

    Favourite POJO generated type

--- src/main/java/com/increff/omni/reporting/pojo/FavouritePojo.java
+++ src/main/java/com/increff/omni/reporting/pojo/FavouritePojo.java
@@ -13,7 +13,7 @@ import jakarta.persistence.*;
 public class FavouritePojo extends AbstractVersionedPojo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)
