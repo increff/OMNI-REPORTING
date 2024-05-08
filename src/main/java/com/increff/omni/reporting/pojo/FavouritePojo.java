96fad7dfdf200d721ab7152d64a83cde87316522
abhaychandna <email@example.com>
2024-05-08 10:42:39 +0530

    GenerationType.IDENTITY for fav pojo

--- src/main/java/com/increff/omni/reporting/pojo/FavouritePojo.java
+++ src/main/java/com/increff/omni/reporting/pojo/FavouritePojo.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class FavouritePojo extends AbstractVersionedPojo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)
