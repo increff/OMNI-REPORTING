8ca58a0ca27da39c75534c94a75cb1a1eaf7aaad
Avishek Chatterjee <avishek.chatterjee@increff.com>
2025-03-10 09:55:37 +0530

    added new future dates

--- src/main/java/com/increff/omni/reporting/model/constants/DynamicDate.java
+++ src/main/java/com/increff/omni/reporting/model/constants/DynamicDate.java
@@ -12,10 +12,20 @@ public enum DynamicDate {
     NOW(),
     TODAY(),
     YESTERDAY(),
-    ONE_WEEK(),
-    FIFTEEN_DAYS(),
+    LAST_WEEK(),
+    FIFTEEN_DAYS_AGO(),
     CURRENT_MONTH(false),
-    LAST_MONTH_1ST();
+    LAST_MONTH_1ST(),
+
+    TOMORROW(),
+    NEXT_WEEK(),
+    FIFTEEN_DAYS_LATER(),
+    NEXT_MONTH_1ST(),
+
+    @Deprecated(since = "2.2.1", forRemoval = true)
+    ONE_WEEK(),
+    @Deprecated(since = "2.2.1", forRemoval = true)
+    FIFTEEN_DAYS();
 
     private Boolean addTimeEndDate = true; // add 23:59 if date type is END_DATE to include end date
 
@@ -37,11 +47,16 @@ public enum DynamicDate {
             case NOW -> zdt;
             case TODAY -> zdt.withHour(0).withMinute(0).withSecond(0).withNano(0);
             case YESTERDAY -> zdt.minusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
-            case ONE_WEEK -> zdt.minusWeeks(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
-            case FIFTEEN_DAYS -> zdt.minusDays(15).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case LAST_WEEK -> zdt.minusWeeks(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case FIFTEEN_DAYS_AGO -> zdt.minusDays(15).withHour(0).withMinute(0).withSecond(0).withNano(0);
             case CURRENT_MONTH -> zdt.withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
             case LAST_MONTH_1ST ->
                     zdt.minusMonths(1).withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case TOMORROW -> zdt.plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case NEXT_WEEK -> zdt.plusWeeks(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case FIFTEEN_DAYS_LATER -> zdt.plusDays(15).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case NEXT_MONTH_1ST -> zdt.plusMonths(1).withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case ONE_WEEK, FIFTEEN_DAYS -> throw new IllegalArgumentException("Deprecated Dynamic date value, Migration needed: " + dynamicDate);
             default -> throw new IllegalArgumentException("Dynamic Date unsupported value: " + dynamicDate);
         };
     }
