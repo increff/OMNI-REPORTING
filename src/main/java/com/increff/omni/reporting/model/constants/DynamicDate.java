6ab7268af63501a62223838832791ebbf210d91c
Avishek Chatterjee <avishek.chatterjee@increff.com>
2025-03-19 16:14:49 +0530

    removed depricated tag

--- src/main/java/com/increff/omni/reporting/model/constants/DynamicDate.java
+++ src/main/java/com/increff/omni/reporting/model/constants/DynamicDate.java
@@ -22,9 +22,8 @@ public enum DynamicDate {
     FIFTEEN_DAYS_LATER(),
     NEXT_MONTH_1ST(),
 
-    @Deprecated(since = "2.2.1", forRemoval = true)
+    //TODO: For removal
     ONE_WEEK(),
-    @Deprecated(since = "2.2.1", forRemoval = true)
     FIFTEEN_DAYS();
 
     private Boolean addTimeEndDate = true; // add 23:59 if date type is END_DATE to include end date
@@ -47,8 +46,8 @@ public enum DynamicDate {
             case NOW -> zdt;
             case TODAY -> zdt.withHour(0).withMinute(0).withSecond(0).withNano(0);
             case YESTERDAY -> zdt.minusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
-            case LAST_WEEK -> zdt.minusWeeks(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
-            case FIFTEEN_DAYS_AGO -> zdt.minusDays(15).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case LAST_WEEK, ONE_WEEK -> zdt.minusWeeks(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+            case FIFTEEN_DAYS_AGO, FIFTEEN_DAYS -> zdt.minusDays(15).withHour(0).withMinute(0).withSecond(0).withNano(0);
             case CURRENT_MONTH -> zdt.withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
             case LAST_MONTH_1ST ->
                     zdt.minusMonths(1).withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
@@ -56,7 +55,6 @@ public enum DynamicDate {
             case NEXT_WEEK -> zdt.plusWeeks(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
             case FIFTEEN_DAYS_LATER -> zdt.plusDays(15).withHour(0).withMinute(0).withSecond(0).withNano(0);
             case NEXT_MONTH_1ST -> zdt.plusMonths(1).withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
-            case ONE_WEEK, FIFTEEN_DAYS -> throw new IllegalArgumentException("Deprecated Dynamic date value, Migration needed: " + dynamicDate);
             default -> throw new IllegalArgumentException("Dynamic Date unsupported value: " + dynamicDate);
         };
     }
